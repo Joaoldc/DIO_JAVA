d156f6a785d2da1490af45d3333b3b4c1b3f6896
Joao Luiz <joao.ldc@hotmail.com>
2022-11-10 19:04:14 -0300

    adicionar aulas Java

---
+++ controleFluxo/controle_fluxo/src/NotaEscolar.java
@@ -0,0 +1,20 @@
+public class NotaEscolar {
+    
+    public static void main(String[] args) {
+
+        int nota = 4;
+        
+        if(nota >= 7)
+        System.out.println("Aprovado!");
+
+        else if(nota >= 5 && nota < 7)
+        System.out.println("Prova de Recuperação");
+
+        else
+        System.out.println("Reprovado");
+    }
+
+   
+
+    
+}
